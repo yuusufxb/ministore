47f4d6da9b6e8e6600b6f8c62d0d9b0f4ec813a9
yuusufxb <youssefmouha98@gmail.com>
2026-01-22 20:03:50 +0100

    some bootstrap style && undynamic button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,29 +18,54 @@ export function Home(){
     
     
     return(
-        <div>
-            <nav>
-                <Link to="/profile">Profile</Link>
-                <Link to="/card">Card</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
-            <div>
-                
-                <h1>MiniStore</h1>
-                {loading && <p>Loading content ...</p>}
-                {products.map((pro)=>(
-                <>
-                    <h3>{pro.title}</h3>
-                    <img src={pro.images[0]} alt="product picture" />    
-                    <p>{pro.price}</p>
-                </>
-                )
-                    
-                )}
-                
-
-
-            </div>
+        <div className="container my-4">
+    {/* Navigation Bar */}
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary mb-4 rounded">
+    <div className="container-fluid">
+        <h1 className="navbar-brand fs-2 m-0">
+            MiniStore
+        </h1>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/profile">Profile</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/card">Card</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+            </ul>
         </div>
+    </div>
+</nav>
+
+
+    {/* Main Content */}
+    <div>
+        <h1 className="mb-4 text-center">MiniStore</h1>
+        {loading && <p className="text-center">Loading content ...</p>}
+
+        <div className="row">
+            {products.map((pro, index) => (
+                <div key={index} className="col-md-4 mb-4">
+                    <div className="card h-100">
+                        <img src={pro.images[0]} className="card-img-top" alt={pro.title} />
+                        <div className="card-body text-center">
+                            <h5 className="card-title">{pro.title}</h5>
+                            <p className="card-text">${pro.price}</p>
+                            <button className="btn btn-secondary">Add to card</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+</div>
+
     );
 }
